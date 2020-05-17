321e4a74b0b9d19ce0f14e044cdf31ce630d3a33
Jaredible <jareddiehl96@gmail.com>
2020-05-16 19:26:57 -0500

    Updated

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ const app = express()
 const port = 3000
 
 app.get('/prices/usd', (req, res) => res.json({
-    "USD": {
-        "BAT": 0.2038,
-        "BTC": 9496.98,
-        "DOGE": 0.002564,
-        "ETH": 201.29,
-        "LTC": 43.58,
-        "XRP": 0.201
+    "data": {
+        "BAT": {
+            "symbol": "BAT",
+            "price": 0.2038
+        },
+        "BTC": {
+            "symbol": "BTC",
+            "price": 9496.98
+        },
+        "DOGE": {
+            "symbol": "DOGE",
+            "price": 0.002564
+        },
+        "ETH": {
+            "symbol": "ETH",
+            "price": 201.29
+        },
+        "LTC": {
+            "symbol": "LTC",
+            "price": 43.58
+        },
+        "XRP": {
+            "symbol": "XRP",
+            "price": 0.201
+        }
     }
 }))
 
